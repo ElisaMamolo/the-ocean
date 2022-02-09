a2a5f3e538079f871a01ec64732c3b36fa0290ca
Christian Kroer Sørensen <dkchkrso@lego.com>
2022-02-09 14:47:14 +0100

    NFT routes to index, NFT Push User Asset

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ require("./config")(app);
 const projectName = "the-ocean";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+//app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,28 @@
 const router = require("express").Router();
 
+const Nft = require("../models/Nft.model.js");
+
 /* GET home page */
-router.get("/", (req, res, next) => {
-  if (req.session.user) {
-    res.render("index", { userInSession: req.session.user });
-  } else {
-    res.render("index");
-  }
-});
+// router.get("/", (req, res, next) => {
+//   if (req.session.user) {
+//     res.render("index", { userInSession: req.session.user });
+//   } else {
+//     res.render("index");
+//   }
+// });
 
-
+ router.get("/", (req, res, next) => {
+  Nft.find()
+    .then((allTheNftsFromDB) => {
+      if (req.session.user) {
+        res.render("index", { userInSession: req.session.user, nfts: allTheNftsFromDB });
+      } else {
+        res.render("index", { nfts: allTheNftsFromDB });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+ });
 
 module.exports = router;
